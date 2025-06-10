c82795802db407e4b9042295b44a3d202521d591
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2025-06-10 00:15:18 -0300

    Update script.js

--- obter_geo_localizacao/js/script.js
+++ obter_geo_localizacao/js/script.js
@@ -1,24 +1,37 @@
-$(document).ready(function() {
-  // Vincula uma função ao evento de clique no elemento com o id "btnGeoLocalizao".
-  $("#btnGeoLocalizao").click(function() {
-    let saida = $("#resultado");
+function inicializar() {
+  let botaoGeoLocalizao = document.getElementById('geoLocalizaoBtn');
+  let saida = document.getElementById('resultado');
 
-    try {
-      // Chama a função getCurrentPosition da API de geolocalização do navegador
-      navigator.geolocation.getCurrentPosition(function successoNaRequisicao(posicao) {
-        mostrarPosicao(posicao, saida);
-      }, function falhaNaRequisicao(err) {
-        saida.html(err.message);
-      });
-    } catch (err) {
-      saida.html(err.message);
-    }
-  });
-
-  function mostrarPosicao(posicao, saida) {
-    saida.html(
-      "Latitude: " + posicao.coords.latitude +
-      "<br>Longitude: " + posicao.coords.longitude
-    );
+  //prettier-ignore
+  if ((botaoGeoLocalizao instanceof HTMLButtonElement) && (saida instanceof HTMLParagraphElement)) {
+    botaoGeoLocalizao.addEventListener('click', () => {
+      try {
+        // Utiliza a API de geolocalização do navegador para obter a posição atual do usuário.
+        navigator.geolocation.getCurrentPosition(
+          (posicao) => {
+            /**
+             * O parâmetro `posicao` é um objeto que contém as coordenadas do usuário
+             * e outras informações adicionais. Ele possui uma propriedade `coords` com:
+             * - `latitude`: A latitude atual do usuário.
+             * - `longitude`: A longitude atual do usuário.
+             */
+            saida.textContent = `Latitude: ${posicao.coords.latitude}, Longitude: ${posicao.coords.longitude}`;
+          },
+          // Função chamada quando a requisição falha.
+          (erro) => {
+            /**
+             * O parâmetro `erro` é um objeto que descreve o motivo da falha.
+             * Ele possui uma propriedade `message` que contém uma descrição da falha.
+             * Exemplo: "User denied Geolocation" (Usuário negou a permissão de localização).
+             */
+            saida.textContent = erro.message;
+          }
+        );
+      } catch (erro) {
+        saida.textContent = erro.message;
+      }
+    });
   }
-});
+}
+
+document.addEventListener('DOMContentLoaded', inicializar);
